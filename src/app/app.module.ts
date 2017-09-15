7d081c04937ec333eec096812f48c390504567b0
knuwu@bluewin.ch <tho3whyj>
2017-09-15 19:57:55 +0200

    - Version mit Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./dashboard.component";
 import { HeroesComponent } from './heroes.component';
-import {HeroDetailComponent} from "./hero-detail.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroService } from "./hero.service";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
+    AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [HeroesComponent]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { OnInit} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
 import { Hero } from './hero'
-import { HeroService} from "./hero.service";
+import { HeroService } from "./hero.service";
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  providers: [HeroService]
 })
 
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
 
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,5 +29,9 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
